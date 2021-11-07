846282b610922dbf88af7614cb2d6cf2fa2d02f6
kjjurenka <56270768+kjjurenka@users.noreply.github.com>
2021-11-07 16:38:13 -0600

    Added in Brick Material and Colors, Added Abandoned Environment Models, Added Standard Asset Models, Added in a few Outdoor Lights

--- 3DGameUnity/Assets/Scripts/MiniMap.cs
+++ 3DGameUnity/Assets/Scripts/MiniMap.cs
@@ -11,11 +11,12 @@ using UnityEngine;
 public class MiniMap : MonoBehaviour
 {
     public Transform player;
+    public int HeadRoom;
 
     private void LateUpdate()
     {
         Vector3 newPos = player.position;//record pos of player
-        newPos.y = transform.position.y;//resets camera y
+        newPos.y = player.position.y + HeadRoom;//resets camera y
         transform.position = newPos;//repositions camera
 
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f); // rotates camera based on player
